88b3c4a7521aa2e37757ec93b8987c21b58eccc1
Sambhav Kothari <sambhavs.email@gmail.com>
2017-08-21 12:40:40 +0530

    Add center co-ordinates as an argument

--- graphics.py
+++ graphics.py
@@ -2,21 +2,23 @@
 import argparse
 import turtle
 
-MAIN = 0
+from collections import namedtuple
+
+Center = namedtuple('Center', ['x', 'y'])
 
 
-def draw_sym(pen, x, y, color="black"):
-    pen.setpos(x, y)
+def draw_sym(pen, x, y, color="black", center=None):
+    pen.setpos(x + center.x, y + center.y)
     pen.dot(1, color)
-    pen.setpos(-x, -y)
+    pen.setpos(-x + center.x, -y + center.y)
     pen.dot(1, color)
 
 
-def draw_curve(pen, length, denominator):
+def draw_curve(pen, length, denominator, center=None):
     x = 0.0
     y = 0.0
 
-    draw_sym(pen, x, y)
+    draw_sym(pen, x, y, center)
 
     critical_point = (denominator / 3) ** 0.5
 
@@ -24,21 +26,20 @@ def draw_curve(pen, length, denominator):
         x += 1
         if y + 0.5 - ((x**3) / denominator) < 0:
             y += 1
-        draw_sym(pen, x, y)
+        draw_sym(pen, x, y, center)
 
     while x < length:
         y += 1
-        print(y - ((x + 0.5)**3) / denominator)
         if y - ((x + 0.5)**3) / denominator > 0:
             x += 1
-        draw_sym(pen, x, y)
+        draw_sym(pen, x, y, center)
 
 
-def draw_curve_incremental(pen, length, denominator):
+def draw_curve_incremental(pen, length, denominator, center=None):
     x = 0.0
     y = 0.0
 
-    draw_sym(pen, x, y)
+    draw_sym(pen, x, y, "red", center)
 
     critical_point = (denominator / 3) ** 0.5
 
@@ -50,7 +51,7 @@ def draw_curve_incremental(pen, length, denominator):
             decision_paremeter += 1
             y += 1
         decision_paremeter -= (3 * (x**2 + x) + 1) / denominator
-        draw_sym(pen, x, y, "red")
+        draw_sym(pen, x, y, "red", center)
 
     x += 0.5
     decision_paremeter = y + 1 - (x**3 / denominator)
@@ -61,7 +62,7 @@ def draw_curve_incremental(pen, length, denominator):
             decision_paremeter -= (3 * (x**2 + x) + 1) / denominator
             x += 1
         decision_paremeter += 1
-        draw_sym(pen, x - 0.5, y, "red")
+        draw_sym(pen, x - 0.5, y, "red", center)
 
 
 def main():
@@ -73,6 +74,10 @@ def main():
     parser.add_argument('--mode', type=int, default=1,
                         help="""Mode with which to rasterize the given curve.
                                 Values: 1 - Incremental 2 - Basic 3 - Comparison""")
+    parser.add_argument('--x', type=int, default=0,
+                        help="""X co-ordinate of center""")
+    parser.add_argument('--y', type=int, default=0,
+                        help="""Y co-ordinate of center""")
     args = parser.parse_args()
     turtle.setup(width=1366, height=768)
     turtle.tracer(0, 0)
@@ -80,10 +85,11 @@ def main():
     pen.up()
     pen.hideturtle()
     pen.speed(0)
+    center = Center(args.x, args.y)
     if args.mode % 2:
-        draw_curve_incremental(pen, args.length, args.denominator)
+        draw_curve_incremental(pen, args.length, args.denominator, center)
     if args.mode > 1:
-        draw_curve(pen, args.length, args.denominator)
+        draw_curve(pen, args.length, args.denominator, center)
     turtle.update()
     turtle.done()
 
